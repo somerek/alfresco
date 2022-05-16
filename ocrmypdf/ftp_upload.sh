ae5a590922fa20cc85543c0966a06cddb5e4414a
somerek <somerek@yandex.ru>
2022-05-16 12:58:00 +0300

    v.1.1

--- ocrmypdf/ftp_upload.sh
+++ ocrmypdf/ftp_upload.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
-
 username=$3
 #password=$4
 bitrixId=$5
 phoneIP=$6
 phoneNum=$7
-filename=`echo $1 | sed 's|.*/\([^/]*\)$|\1|'`
-filetxt=`echo $1 | sed -r 's/\.[[:alnum:]]+$/.txt/'`
-filepath=`echo "${2%/*}" | cut -d '/' -f 4-`
 
 word2=`echo "$1" |  cut -d '/' -f 3`
 word3=`echo "$1" |  cut -d '/' -f 4`
 
 if [ "$word2" == "berezhnaya_ov" ] && [ "$word3" == "FAX" ]; then
+filetxt=`echo $1 | sed -r 's/\.[[:alnum:]]+$/.txt/'`
 ncftpput -u $alfresco_admin -p $alfresco_password -P 2221 -m alfresco "/Alfresco/User Homes/$username/FAX" "$2"
 cat "$filetxt" | mutt -s "FAX" -e 'my_hdr From: FAX server <postmaster@edc-electronics.ru>' -a "$2" -- fax@mail.edc-electronics.ru
 # Clean
-sudo rm "$1"
 sudo rm "$filetxt"
-rm "$2"
 rm sent
 else
 
 # NOT FAX
+filepath=`echo "${2%/*}" | cut -d '/' -f 4-`
+filename=`basename "$1"`
 # Upload output file to ftp alfresco
 ncftpput -u $alfresco_admin -p $alfresco_password -P 2221 -m alfresco "/Alfresco/User Homes/$username/scan/$filepath" "$2"
 
+# Notify to bitrix
+curl -s -k $bitrix_url -F MESSAGE="Scanned: $filename" -F DIALOG_ID=$bitrixId
+
 # Notify to asterisk
 ping -q -c1 $phoneIP > /dev/null
-
 if [ $? -eq 0 ]
 then
 cp /app/scan.call /tmp/scan$username.call
@@ -37,12 +36,10 @@ scp -i /ftp_acc/root@asterisk -o StrictHostKeyChecking=no -o UserKnownHostsFile=
 rm /tmp/scan$username.call
 fi
 
-# Notify to bitrix
-curl -s -k $bitrix_url -F MESSAGE="Scaned: $filename" -F DIALOG_ID=$bitrixId
+fi
 
 # Clean
 sudo rm "$1"
 rm "$2"
-fi
 
 exit 0 # OK!
